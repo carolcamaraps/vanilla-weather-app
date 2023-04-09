9ef9a0e61fad1964e9f4b2d2fcb735d19cd4b11f
carolcamaraps <carol@carolinacamara.com>
2023-04-09 20:49:43 -0300

    added time update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = String(date.getHours()).padStart(2, "0");
+let minutes = String(date.getMinutes()).padStart(2, "0");
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
  console.log(response.data); 
   let temperatureElement = document.querySelector("#temperature");
@@ -5,16 +15,18 @@ function displayTemperature(response) {
   let descpriptionElement = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descpriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 
 }
 
 let apiKey = "74207tf2dbea64540ea5c6f390o295e3";
-let query = "London";
+let query = "Rio de Janeiro";
 
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
 
